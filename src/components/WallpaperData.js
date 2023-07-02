067f9672ad23455404fb416c0af28ed70868d37f
blackadamlol <ssahilpreet35@gmail.com>
2023-07-02 12:45:59 +0100

    done images

--- src/components/WallpaperData.js
+++ src/components/WallpaperData.js
@@ -2,185 +2,330 @@ import minato from "../images/minato (2).jpg"
 import minxkush from "../images/minato-kushina.jpg"
 import minato2 from "../images/minato.jpg"
 import naruto from "../images/naruto.jpg"
+import whitebeard from "../images/whitebeard.jpg"
+import usopp from "../images/usopp.jpg"
+import shanks4 from "../images/shanks4.jpg"
+import shanks3 from "../images/shanks3.jpg"
+import shanks1 from "../images/shanks1.jpg"
+import shanks from "../images/shanks.jpg"
+import sakura from "../images/sakura.jpg"
+import sasuke from "../images/sasuke.jpg"
+import sanji from "../images/sanji.webp"
+import sabo1 from "../images/sabo2.jpg"
+import sabo2 from "../images/sabo2.jpg"
+import sabo from "../images/sabo.jpg"
+import obito from "../images/obito1.jpg"
+import obito1 from "../images/obito.webp"
+import narutoxsasuke from "../images/narutovsasuke.jpg"
+import nami from "../images/nami.jpg"
+import mihawk from "../images/mihawk6.jpg"
+import mihawk1 from "../images/mihawk5.jpg"
+import mihawk2 from "../images/mihawk4.jpg"
+import mihawk3 from "../images/mihawk3.jpg"
+import mihawk4 from "../images/mihawk2.jpeg"
+import mihawk5 from "../images/mihawk.png"
+import luffy from "../images/luffy.jpg"
+import jiraya from "../images/jiraya1.jpg"
+import jiraya1 from "../images/jiraya.jpg"
+import itachi from "../images/itachi.jpg"
+import goldroger from "../images/gol d roger.jpg"
+import garps from "../images/garps.jpg"
+import doflamingo from "../images/doflamingo.jpg"
+import doflamingo1 from "../images/doflamingo1.jpg"
+import doflamingo2 from "../images/doflamingo2.jpg"
+import doflamingo3 from "../images/doflamingo3.jpg"
+import doflamingo4 from "../images/doflamingo4.jpg"
+import doflamingo5 from "../images/doflamingo5.jpg"
+import chopper from "../images/chopper.jpg"
+import luffyxsaboxace from "../images/chopper.jpg"
+import brook from "../images/Brook.jpg"
+import blackbeard from "../images/blackbeard.jpg"
+import ace from "../images/ace.jpg"
+import ace1 from "../images/ace1.jpg"
+import ace2 from "../images/ace2.jpg"
+import ace3 from "../images/ace3.jpg"
+import ace4 from "../images/ace4.jpg"
+import ace5 from "../images/ace5.jpg"
+import ace6 from "../images/ace6.jpg"
+import ace7 from "../images/ace7.png"
+
 const WallpaperData = [
-    {
-      series: 'Naruto',
-      characters: [
-        {
-          name: 'Naruto Uzumaki',
-          img: naruto,
-        },
-        {
-          name: 'Sasuke Uchiha',
-          img: 'https://example.com/sasuke.jpg',
-        },
-        {
-          name: 'Sakura Haruno',
-          img: 'https://example.com/sakura.jpg',
-        },
-        {
-          name: 'Kakashi Hatake',
-          img: 'https://example.com/kakashi.jpg',
-        },
-        {
-          name: 'Itachi Uchiha',
-          img: 'https://example.com/itachi.jpg',
-        },
-        {
-          name: 'Hinata Hyuga',
-          img: 'https://example.com/hinata.jpg',
-        },
-        {
-          name: 'Shikamaru Nara',
-          img: 'https://example.com/shikamaru.jpg',
-        },
-        {
-          name: 'Rock Lee',
-          img: 'https://example.com/rocklee.jpg',
-        },
-        {
-          name: 'Neji Hyuga',
-          img: 'https://example.com/neji.jpg',
-        },
-        {
-          name: 'Gaara',
-          img: 'https://example.com/gaara.jpg',
-        },
-        {
-          name: 'Jiraiya',
-          img: 'https://example.com/jiraiya.jpg',
-        },
-        {
-          name: 'Tsunade',
-          img: 'https://example.com/tsunade.jpg',
-        },
-        {
-          name: 'Orochimaru',
-          img: 'https://example.com/orochimaru.jpg',
-        },
-        {
-          name: 'Minato Namikaze',
-          img: minato,
-        },
-        {
-          name: "Minato Namikaze 2",
-          img: minato2,
-        },
-        {
-          name: 'Kushina Uzumaki',
-          img: 'https://example.com/kushina.jpg',
-        },
-        {
-          name: 'Shisui Uchiha',
-          img: 'https://example.com/shisui.jpg',
-        },
-        {
-          name: 'Tobirama Senju',
-          img: 'https://example.com/tobirama.jpg',
-        },
-        {
-          name: 'Hiruzen Sarutobi',
-          img: 'https://example.com/hiruzen.jpg',
-        },
-        {
-          name: 'Madara Uchiha',
-          img: 'https://example.com/madara.jpg',
-        },
-        {
-          name: 'Hashirama Senju',
-          img: 'https://example.com/hashirama.jpg',
-        },
-      ],
-      genre: 'Action, Adventure, Fantasy',
-    },
-    {
-      series: 'One Piece',
-      characters: [
-        {
-          name: 'Monkey D. Luffy',
-          img: 'https://example.com/luffy.jpg',
-        },
-        {
-          name: 'Roronoa Zoro',
-          img: 'https://example.com/zoro.jpg',
-        },
-        {
-          name: 'Nami',
-          img: 'https://example.com/nami.jpg',
-        },
-        {
-          name: 'Sanji',
-          img: 'https://example.com/sanji.jpg',
-        },
-        {
-          name: 'Tony Tony Chopper',
-          img: 'https://example.com/chopper.jpg',
-        },
-        {
-          name: 'Nico Robin',
-          img: 'https://example.com/robin.jpg',
-        },
-        {
-          name: 'Franky',
-          img: 'https://example.com/franky.jpg',
-        },
-        {
-          name: 'Brook',
-          img: 'https://example.com/brook.jpg',
-        },
-        {
-          name: 'Usopp',
-          img: 'https://example.com/usopp.jpg',
-        },
-        {
-          name: 'Jinbe',
-          img: 'https://example.com/jinbe.jpg',
-        },
-        {
-          name: 'Portgas D. Ace',
-          img: 'https://example.com/ace.jpg',
-        },
-        {
-          name: 'Boa Hancock',
-          img: 'https://example.com/hancock.jpg',
-        },
-        {
-          name: 'Trafalgar D. Water Law',
-          img: 'https://example.com/law.jpg',
-        },
-        {
-          name: 'Eustass Kid',
-          img: 'https://example.com/kid.jpg',
-        },
-        {
-          name: 'Dracule Mihawk',
-          img: 'https://example.com/mihawk.jpg',
-        },
-        {
-          name: 'Shanks',
-          img: 'https://example.com/shanks.jpg',
-        },
-        {
-          name: 'Blackbeard',
-          img: 'https://example.com/blackbeard.jpg',
-        },
-        {
-          name: 'Buggy',
-          img: 'https://example.com/buggy.jpg',
-        },
-        {
-          name: 'Kuzan',
-          img: 'https://example.com/kuzan.jpg',
-        },
-        {
-          name: 'Akainu',
-          img: 'https://example.com/akainu.jpg',
-        },
-      ],
-      genre: 'Action, Adventure, Comedy',
-    },
-    // Add more series objects as needed
-  ];
-  
-  export default WallpaperData;
-  
+  {
+    series: 'Naruto',
+    characters: [
+      {
+        name: 'Naruto Uzumaki',
+        img: naruto,
+      },
+      {
+        name: 'Sasuke Uchiha',
+        img: sasuke,
+      },
+      {
+        name: 'Sakura Haruno',
+        img: sakura,
+      },
+      {
+        name: 'Kakashi Hatake',
+        img: 'https://example.com/kakashi.jpg',
+      },
+      {
+        name: 'Itachi Uchiha',
+        img: 'https://example.com/itachi.jpg',
+      },
+      {
+        name: 'Hinata Hyuga',
+        img: 'https://example.com/hinata.jpg',
+      },
+      {
+        name: 'Shikamaru Nara',
+        img: 'https://example.com/shikamaru.jpg',
+      },
+      {
+        name: 'Obito Uchiha',
+        img: obito,
+      },
+      {
+        name: 'Obito Uchiha 2',
+        img: obito1,
+      },
+      {
+        name: 'Neji Hyuga',
+        img: 'https://example.com/neji.jpg',
+      },
+      {
+        name: 'Gaara',
+        img: 'https://example.com/gaara.jpg',
+      },
+      {
+        name: 'Jiraiya',
+        img: 'https://example.com/jiraiya.jpg',
+      },
+      {
+        name: 'Tsunade',
+        img: 'https://example.com/tsunade.jpg',
+      },
+      {
+        name: 'Orochimaru',
+        img: 'https://example.com/orochimaru.jpg',
+      },
+      {
+        name: 'Minato Namikaze',
+        img: minato,
+      },
+      {
+        name: "Minato Namikaze 2",
+        img: minato2,
+      },
+      {
+        name: "Minato Namikaze 3",
+        img: minxkush,
+      },
+      {
+        name: 'Kushina Uzumaki',
+        img: 'https://example.com/kushina.jpg',
+      },
+      {
+        name: 'Shisui Uchiha',
+        img: 'https://example.com/shisui.jpg',
+      },
+      {
+        name: 'Tobirama Senju',
+        img: 'https://example.com/tobirama.jpg',
+      },
+      {
+        name: 'Hiruzen Sarutobi',
+        img: 'https://example.com/hiruzen.jpg',
+      },
+      {
+        name: 'Madara Uchiha',
+        img: 'https://example.com/madara.jpg',
+      },
+      {
+        name: 'Hashirama Senju',
+        img: 'https://example.com/hashirama.jpg',
+      },
+    ],
+    genre: 'Action, Adventure, Fantasy',
+  },
+  {
+    series: 'One Piece',
+    characters: [
+      {
+        name: 'Monkey D. Luffy',
+        img: luffy,
+      },
+      {
+        name: 'Monkey D. Luffy',
+        img: luffyxsaboxace,
+      },
+      {
+        name: "Monkey D. Garps",
+        img: garps,
+      },
+      {
+        name: 'Roronoa Zoro',
+        img: 'https://example.com/zoro.jpg',
+      },
+      {
+        name: 'Nami',
+        img: nami,
+      },
+      {
+        name: 'Sanji',
+        img: sanji,
+      },
+      {
+        name: 'Tony Tony Chopper',
+        img: chopper,
+      },
+      {
+        name: 'Nico Robin',
+        img: 'https://example.com/robin.jpg',
+      },
+      {
+        name: 'Franky',
+        img: 'https://example.com/franky.jpg',
+      },
+      {
+        name: 'Brook',
+        img: brook,
+      },
+      {
+        name: 'Usopp',
+        img: usopp,
+      },
+      {
+        name: 'Jinbe',
+        img: 'https://example.com/jinbe.jpg',
+      },
+      {
+        name: 'Portgas D. Ace',
+        img: ace,
+      },
+      {
+        name: 'Portgas D. Ace 1',
+        img: ace1,
+      },
+      {
+        name: 'Portgas D. Ace 2',
+        img: ace2,
+      },
+      {
+        name: 'Portgas D. Ace 3',
+        img: ace3,
+      },
+      {
+        name: 'Portgas D. Ace 4',
+        img: ace4,
+      },
+      {
+        name: 'Portgas D. Ace 5',
+        img: ace5,
+      },
+      {
+        name: 'Portgas D. Ace 6',
+        img: ace6,
+      },
+      {
+        name: 'Portgas D. Ace 7',
+        img: ace7,
+      },
+      {
+        name: 'Whitebeard',
+        img: whitebeard,
+      },
+      {
+        name: 'Shanks',
+        img: shanks,
+      },
+      {
+        name: "Sabo",
+        img: sabo,
+      },
+      {
+        name: "Sabo1",
+        img: sabo1,
+      },
+      {
+        name: "Sabo2",
+        img: sabo2,
+      },
+      {
+        name: 'Shanks 1',
+        img: shanks1,
+      },
+      {
+        name: 'Shanks 3',
+        img: shanks3,
+      },
+      {
+        name: 'Shanks 4',
+        img: shanks4,
+      },
+      {
+        name: 'Blackbeard',
+        img: blackbeard,
+      },
+      {
+        name: 'Mihawk',
+        img: mihawk,
+      },
+      {
+        name: 'Mihawk 1',
+        img: mihawk1,
+      },
+      {
+        name: 'Mihawk 2',
+        img: mihawk2,
+      },
+      {
+        name: 'Mihawk 3',
+        img: mihawk3,
+      },
+      {
+        name: 'Mihawk 4',
+        img: mihawk4,
+      },
+      {
+        name: 'Mihawk 5',
+        img: mihawk5,
+      },
+      {
+        name: 'Gol D. Roger',
+        img: goldroger,
+      },
+      {
+        name: 'Emporio Ivankov',
+        img: 'https://example.com/ivankov.jpg',
+      },
+      {
+        name: 'Donquixote Doflamingo',
+        img: doflamingo,
+      },
+      {
+        name: 'Donquixote Doflamingo 1',
+        img: doflamingo1,
+      },
+      {
+        name: 'Donquixote Doflamingo 2',
+        img: doflamingo2,
+      },
+      {
+        name: 'Donquixote Doflamingo 3',
+        img: doflamingo3,
+      },
+      {
+        name: 'Donquixote Doflamingo 4',
+        img: doflamingo4,
+      },
+      {
+        name: 'Donquixote Doflamingo 5',
+        img: doflamingo5,
+      },
+    ],
+    genre: 'Action, Adventure, Comedy',
+  },
+];
+
+export default WallpaperData;
