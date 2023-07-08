4531addd08fbedfecbde4af22463666b51b7187d
blackadamlol <ssahilpreet35@gmail.com>
2023-07-08 19:29:15 +0100

    did some more cs need some finish and add more pic

--- src/components/WallpaperComponent.js
+++ src/components/WallpaperComponent.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import WallpaperData from './WallpaperData';
 import stringSimilarity from 'string-similarity';
-import './WallpaperComponent.css'; // Import the CSS file for styling
+import './WallpaperComponent.css';
 
 const WallpaperComponent = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -90,6 +90,13 @@ const WallpaperComponent = () => {
     }
   };
 
+  const handleDownloadImage = (image) => {
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'wallpaper';
+    link.click();
+  };
+
   const handleViewImage = (image) => {
     setSelectedImage(image);
   };
@@ -98,69 +105,81 @@ const WallpaperComponent = () => {
     setSelectedImage(null);
   };
 
-  return (
-    <>
-      <div className='home-background'></div>
-      <div className="wallpaper-container">
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Search series, genre, or character..."
-            value={searchTerm}
-            onChange={handleSearchChange}
-          />
-          <button onClick={handleSearchButtonClick}>Search</button>
-        </div>
+return (
+  <>
+    <div className="top-image"></div>
+    <div className="wallpaper-container">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search series, genre, or character..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <button onClick={handleSearchButtonClick}>Search</button>
+      </div>
 
-        {loading && <p>Loading...</p>}
+      {loading && <p>Loading...</p>}
 
-        {searchTerm.length >= 3 && searchResults.length === 0 && (
-          <h3>{searchTerm} not found. Showing random wallpapers.</h3>
-        )}
+      {searchTerm.length >= 3 && searchResults.length === 0 && (
+        <h3>{searchTerm} not found. Showing random wallpapers.</h3>
+      )}
 
-        <div className="image-container">
-          {searchResults.map((character, index) => (
-            <div className="image-item" key={index}>
-              <img
-                src={character.img}
-                alt={character.name}
-                onClick={() => handleViewImage(character.img)}
-              />
-              <div className="image-overlay">
-                <h4>{character.name}</h4>
-                <p>{character.series}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className="image-container">
-          {randomPictures.map((character, index) => (
-            <div className="image-item" key={index}>
-              <img
-                src={character.img}
-                alt={character.name}
-                onClick={() => handleViewImage(character.img)}
-              />
-              <div className="image-overlay">
-                <h4>{character.name}</h4>
-                <p>{character.series}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        {selectedImage && (
-          <div className="modal">
-            <div className="modal-content">
-              <span className="close" onClick={handleCloseModal}>&times;</span>
-              <img src={selectedImage} alt="Full View" className="full-view-image" />
+      <div className="image-container">
+        {searchResults.map((character, index) => (
+          <div className="image-item" key={index}>
+            <img
+              src={character.img}
+              alt={character.name}
+              onClick={() => handleViewImage(character.img)}
+            />
+            <div className="image-overlay">
+              <h4>{character.name}</h4>
+              <p>{character.series}</p>
+              <button
+                className="download-button"
+                onClick={() => handleDownloadImage(character.img)}
+              >
+                Download
+              </button>
             </div>
           </div>
-        )}
+        ))}
       </div>
-    </>
-  );
+
+      <div className="image-container">
+        {randomPictures.map((character, index) => (
+          <div className="image-item" key={index}>
+            <img
+              src={character.img}
+              alt={character.name}
+              onClick={() => handleViewImage(character.img)}
+            />
+            <div className="image-overlay">
+              <h4>{character.name}</h4>
+              <p>{character.series}</p>
+              <button
+                className="download-button"
+                onClick={() => handleDownloadImage(character.img)}
+              >
+                Download
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {selectedImage && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleCloseModal}>&times;</span>
+            <img src={selectedImage} alt="Full View" className="full-view-image" />
+          </div>
+        </div>
+      )}
+    </div>
+  </>
+);
 };
 
 export default WallpaperComponent;
